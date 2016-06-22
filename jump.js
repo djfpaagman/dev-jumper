61ac2871d08bb69a0aed79516fcd273c71ab8972
Dennis Paagman <dennispaagman@gmail.com>
2016-06-22 21:17:43 +0200

    Clean up and add comments

--- jump.js
+++ jump.js
@@ -1,20 +1,17 @@
 chrome.commands.onCommand.addListener(function(command) {
-  switch(command) {
-    case "jump-to-from-dev":
-      jumpToOrFromDev();
-      break;
+  if (command === "jump-to-from-dev") {
+    jumpToOrFromDev();
   }
 });
 
-function jumpToOrFromDev(direction) {
+function jumpToOrFromDev() {
   chrome.tabs.query({ active: true }, function(tab) {
     active_tab = tab[0];
     url = parseUrl(active_tab.url);
-    id = active_tab.id;
 
     if (isDevEnv(url.hostname)) {
       // Switch to production
-      url.hostname = url.hostname.substring(0, url.hostname.length - 4);
+      url.hostname = url.hostname.slice(0, -4);
       url.protocol = "https:";
       url.port = "443";
     } else {
@@ -24,11 +21,14 @@ function jumpToOrFromDev(direction) {
       url.port = "3000";
     }
 
-    chrome.tabs.update(id, { url: url.href });
+    // Change the URL of the active tab to other environment
+    chrome.tabs.update(active_tab.id, { url: url.href });
   });
 }
 
 function parseUrl(url) {
+  // Parses the URL so we can access each element (hostname, protocol, port)
+  // individually without using stuff like regex magic.
   var parser = document.createElement("a");
   parser.href = url;
 
@@ -36,5 +36,6 @@ function parseUrl(url) {
 }
 
 function isDevEnv(hostname) {
-  return (hostname.substr(hostname.length - 4) === ".dev")
+  // A hostname is consider the dev environment when it ends with .dev
+  return (hostname.slice(-4) === ".dev")
 }
